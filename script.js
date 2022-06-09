4e75ff98f207869a7535ebaef09420e0d5b27f16
rezi93 <103065023+rezi93@users.noreply.github.com>
2022-06-09 21:27:25 +0400

    add js

--- script.js
+++ script.js
@@ -9,3 +9,43 @@ burger.addEventListener('click', function(){
 })
 
 
+$('.slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+    ]
+  });
+responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+]
+
+
